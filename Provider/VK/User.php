c164b87bec9b527905bc440fc44255f02114c3b5
FreeFly19 <you@example.com>
2015-10-16 19:04:18 +0300

    add VK wrapper service

---
+++ Provider/VK/User.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Provider\VK;
+
+class User{
+    public $first_name;
+
+    public $last_name;
+
+}
--- web/index.php
+++ web/index.php
@@ -2,11 +2,22 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-$app = new Silex\Application();
+//Костиль :Р
+require_once __DIR__.'/../Provider/VK/VKService.php';
+require_once __DIR__.'/../Provider/VK/User.php';
+require_once __DIR__.'/../Provider/VK/Post.php';
+//КостильEND
 
+
+$app = new Silex\Application();
+$vk = new Provider\VK\VKService(new \VK\VK(null, null));
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
+
+$app->get('/group/{id}/posts', function ($id) use ($app,$vk) {
+    return json_encode($vk->getGroupPosts($id));
+});
 
 $app->run();
